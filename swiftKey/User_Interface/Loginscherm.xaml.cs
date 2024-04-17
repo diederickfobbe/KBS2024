358c56bc892e452591096a8b4a5c31868cb1c556
Max <maxgerbens@gmail.com>
2024-04-17 13:48:08 +0200

    Inlogscherm en Oefenscherm toegevoegd

---
+++ swiftKey/User_Interface/Loginscherm.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace User_Interface
+{
+    public partial class Loginscherm : ContentPage {
+
+        public Loginscherm()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Button_OnClicked(object? sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Oefenscherm());
+        }
+    }
+
+}
