7f1f55db8dafe7815a090415833d67e46d133ee1
Max <maxgerbens@gmail.com>
2024-05-13 14:32:06 +0200

    naam van game naar oefening veranderd

--- swiftKey/User_Interface/Schermen/SelecterenOefening.xaml.cs
+++ swiftKey/User_Interface/Schermen/SelecterenOefening.xaml.cs
@@ -3,37 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
 
-namespace User_Interface.Schermen;
-
-public partial class SelecterenOefening : ContentPage {
-
-    public SelecterenOefening()
+namespace User_Interface.Schermen
+{
+    public partial class SelecterenOefening : ContentPage
     {
-        InitializeComponent();
-
-
-        List<Game> Games = new List<Game>()
+        public SelecterenOefening()
         {
-            new Game{Name="game1", Difficulty="Hard", ImageLocation="swiftkey.png"},
-            new Game{Name="game2", Difficulty="Easy",ImageLocation="ukimage.png"},
-            new Game{Name="game3", Difficulty="medium",ImageLocation="ukimage.png"}
+            InitializeComponent();
+            InitializeOefeningen();
+        }
 
-        };
-        SelectOefeningen.ItemsSource = Games;
-           
+        private void InitializeOefeningen()
+        {
+            List<Oefening> Games = new List<Oefening>()
+            {
+                new Oefening{Name="game1", Difficulty="Hard", ImageLocation="swiftkey.png"},
+                new Oefening{Name="game2", Difficulty="Easy",ImageLocation="ukimage.png"},
+                new Oefening{Name="game3", Difficulty="medium",ImageLocation="ukimage.png"}
+            };
+            SelectOefeningen.ItemsSource = Games;
+        }
+    }
+
+    public class Oefening
+    {
+        public string Name { get; set; }
+        public string Difficulty { get; set; }
+        public string ImageLocation { get; set; }
     }
 }
-
-
-public class Game
-{
-    public string Name{ get; set; }
-    public string Difficulty { get; set; }
-
-    public string ImageLocation { get; set; }
-
-
-}
-
-
