9c20744cf06ebde140a4add49de744639b25b69f
Donny <donnyspek@gmail.com>
2024-05-16 18:15:09 +0200

    US: Moeilijkheidgraad
    Moeilijkheidsgraad heeft nu een kleurtje met wat kleine aanpassingen.

---
+++ swiftKey/User_Interface/Schermen/DifficultyToColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Controls;
+
+namespace User_Interface.Converters
+{
+    public class DifficultyToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string difficulty)
+            {
+                switch (difficulty.ToLower())
+                {
+                    case "hard":
+                        return Colors.Red;
+                    case "medium":
+                        return Colors.Orange;
+                    case "easy":
+                        return Colors.Green;
+                    default:
+                        return Colors.Gray; // Default color if no match found
+                }
+            }
+            return Colors.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
